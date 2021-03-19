fd61955c3c0d32a8f3e31c0b0e4e697c66589ee7
Youhei Nozaki <youhey.hey0505@gmail.com>
2021-03-19 10:35:08 +0900

    firebase init

--- plugins/firebase.ts
+++ plugins/firebase.ts
@@ -6,12 +6,12 @@ export default defineNuxtPlugin(() => {
     return
   }
   firebase.initializeApp({
-    apiKey: 'AIzaSyA3PqKsxdRHSdbO6IuferqfKC7rMXuZCCU',
-    authDomain: 'senseitime-a0c8d.firebaseapp.com',
-    projectId: 'senseitime-a0c8d',
-    storageBucket: 'senseitime-a0c8d.appspot.com',
-    messagingSenderId: '409589300715',
-    appId: '1:409589300715:web:985f3841adca55c273adae',
-    measurementId: 'G-MYE9M95JJ2',
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID,
+    measurementId: process.env.FIREBASE_MEASUREMENT_ID,
   })
 })
