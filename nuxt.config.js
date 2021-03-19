557ae905a0a8d69b6665bb326d937cb6e4b72bee
Youhei Nozaki <youhey.hey0505@gmail.com>
2021-03-19 11:34:59 +0900

    fix

--- nuxt.config.js
+++ nuxt.config.js
@@ -18,7 +18,7 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
-    { src: '~/plugins/composition-api.js' },
+    { src: '~/plugins/composition-api.ts' },
     { src: '~/plugins/firebase.ts' },
   ],
   env: {
---
+++ types/user.ts
@@ -0,0 +1,7 @@
+export type User = {
+  displayName: string
+  email: string
+  photoURL: string
+  // providerId: string
+  uid: string
+}
