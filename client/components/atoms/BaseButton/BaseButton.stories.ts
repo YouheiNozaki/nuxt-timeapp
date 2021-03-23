8c7f55cae8da4ea7b4646683175cf977399bb57c
Youhei Nozaki <youhey.hey0505@gmail.com>
2021-03-23 17:02:23 +0900

    fix: setting storybook

---
+++ client/components/atoms/BaseButton/BaseButton.stories.ts
@@ -0,0 +1,10 @@
+import BaseButton from './index.vue'
+
+export default {
+  title: 'atoms/BaseButton',
+}
+
+export const Normal = () => ({
+  components: { BaseButton },
+  template: '<BaseButton/>',
+})
