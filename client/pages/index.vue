9168e18ee9c7dc9d7bed2d6c8723a73834b96536
Youhei Nozaki <youhey.hey0505@gmail.com>
2021-03-23 11:52:50 +0900

    fix: uses nuxtjs/composition-api

--- client/pages/index.vue
+++ client/pages/index.vue
@@ -5,11 +5,11 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, ref } from '@vue/composition-api'
+import { defineComponent, ref } from '@nuxtjs/composition-api'
 
 export default defineComponent({
   setup() {
-    const message = ref('Hello World')
+    const message = ref('This is a message')
 
     return {
       message,
